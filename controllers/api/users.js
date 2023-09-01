7ef169f88b3205c8d9da3b121c01b8d587e1c3fe
tinanvo <tinanhivo@gmail.com>
2023-09-01 15:30:26 +1000

    Create JSON Web Token that includes user's data

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,10 +1,28 @@
-function create(req, res) {
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email
-    }
-  });
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+
+async function create(req, res) {
+  try {
+    // Add the user to the database
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    
+    res.json(token);
+  } catch (err) {
+    // Client will check for non-2xx status code 
+    // 400 = Bad Request
+    res.status(400).json(err);
+  }
+}
+
+/*-- Helper Functions --*/
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  );
 }
 
 module.exports = {
